00aedc18f351f782488a0ed90f03d7d13561b37d
Ra1der69 <ra1derplays01@gmail.com>
2026-02-23 18:52:13 +0000

    work
    username password and search

--- V-ObjectOrenttated/V-ObjectOrenttated.cpp
+++ V-ObjectOrenttated/V-ObjectOrenttated.cpp
@@ -5,14 +5,48 @@
 #include <string>
 
 using namespace std; 
+
+string Response;
+string Book;
+string USERNAME;
+float DOB;
+string PASSWORD;
+
+bool isAvalible = true;
+bool isUnAvalible = false;
+
+
+
+
 int main()
 {
-     cout << "Would you like to rent a book?\n";
+    cout << " log in using your USERNAME and DOB\n";
+    
+
+    cout << "USERNAME: "; cin >> USERNAME; 
+    cout << "DOB: ";  cin >> DOB; 
+    cout << "PASSWORD: ";  cin >> PASSWORD;
+    cout << endl;
+    cout << "USERNAME:" << USERNAME << "\n"; 
+    cout << "D0B:" << DOB << "\n"; 
+    cout << "PASSWORD: " << "********* \n";
+    
+    cout << "thank you for logging in\n would you like to rent a book? (YES or NO) \n " ;
+    cin >> Response;
+
+    if (Response == "yes")
+    {
+        cout << "what book would you like to rent? \n";
+
+        cin >> Book ;
+
+        cout << "searching if "; cout << Book << " is avalible\n";
+    }
+
+    else {
+        cout << "try again" << endl;
+    }
 
 
-     string Book = isAvalible;
 
-     bool isAvalible = true;
-     bool isUnAvalible = false;
 }
-
