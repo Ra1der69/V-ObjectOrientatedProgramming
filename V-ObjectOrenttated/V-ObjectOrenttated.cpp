b71107b9666c5d941c70c9618c1e9ef0942bd2ce
Ra1der69 <ra1derplays01@gmail.com>
2026-02-23 12:43:05 +0000

    Add project files.

---
+++ V-ObjectOrenttated/V-ObjectOrenttated.cpp
@@ -0,0 +1,18 @@
+// V-ObjectOrenttated.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std; 
+int main()
+{
+     cout << "Would you like to rent a book?\n";
+
+
+     string Book = isAvalible;
+
+     bool isAvalible = true;
+     bool isUnAvalible = false;
+}
+
